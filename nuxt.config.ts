cbc06a05b0a2b2e14977c619887c12f230069f9e
David Arenas <davejs136@gmail.com>
2023-07-06 10:00:53 -0400

    :tada: initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,60 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  telemetry: false,
+  app: {
+    head: {},
+    pageTransition: { name: "page", mode: "out-in" },
+    layoutTransition: { name: "layout", mode: "out-in" },
+  },
+
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@kevinmarrec/nuxt-pwa",
+    "nuxt-lodash",
+    "@nuxt/image-edge",
+  ],
+
+  lodash: {
+    prefix: "$",
+    upperAfterPrefix: false,
+  },
+
+  pwa: {
+    meta: {
+      title: "Eurogrober",
+      name: "Eurogrober Web App",
+      author: "Novanet Studio <info@novanet.studio>",
+      description: "Landing page for eurogrober company",
+      theme_color: "#31150C",
+      lang: "es",
+    },
+    manifest: {
+      name: "Eurogrober Web App",
+      short_name: "Eurogrober",
+      description: "Landing page for eurogrober company",
+      start_url: "/",
+      display: "standalone",
+      background_color: "#ffffff",
+      theme_color: "#31150C",
+    },
+    icon: { source: "static/favicon.png" },
+    /*workbox: {
+      enabled: true,
+    },*/
+  },
+
+  image: {
+    // The screen sizes predefined by `@nuxt/image`:
+    screens: {
+      xs: 320,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      xxl: 1536,
+      "2xl": 1536,
+    },
+  },
+
+  devtools: { enabled: true },
+});
