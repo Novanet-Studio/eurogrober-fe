456a5c9155dd3e1ef019ac76455228c6c9c453ae
David Arenas <davejs136@gmail.com>
2023-07-11 11:17:55 -0400

    refactor: gallery-section

--- components/gallery-section.vue
+++ components/gallery-section.vue
@@ -1,51 +1,32 @@
 <template>
   <section>
     <div class="container relative md:mt-24 mt-16">
-      <div class="grid grid-cols-1 pb-8 text-center">
-        <h3
-          class="mb-4 md:text-3xl md:leading-normal text-2xl leading-normal font-semibold"
-        >
-          Our Services
-        </h3>
-
-        <p class="text-slate-400 max-w-xl mx-auto">
-          Start working with Tailwind CSS that can provide everything you need
-          to generate awareness, drive traffic, connect.
-        </p>
-      </div>
-      <!--end grid-->
-
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla,
-        deserunt quidem. Similique, ducimus quam, adipisci maiores modi, facere
-        facilis nesciunt ipsum aspernatur magnam accusamus? Delectus, commodi
-        eaque labore a aliquam, animi repudiandae odio deleniti voluptatem,
-        perferendis voluptas. Consectetur, distinctio quae!
+      <header-title title="Gallery" subtitle="Kitchen possibilities" />
+      <p class="text-color-4 px-6 text-center">
+        We not only offer products. We offer complete solutions for kitchens,
+        with the best products, cutting-edge technological and durable
+        accessories
       </p>
     </div>
-    <section class="relative mt-20">
+    <section class="relative mt-6">
       <div class="container-fluid relative">
         <div class="relative py-24 table w-full shadow-md overflow-hidden">
           <div
             class="absolute inset-0 bg-[url('/images/app/bg.jpg')] bg-no-repeat md:bg-left bg-center bg-cover"
-          ></div>
-          <div class="absolute inset-0 bg-slate-950/30"></div>
-          <div class="container relative">
-            <div class="grid grid-cols-1">
-              <div class="md:text-start text-center">
-                <p
-                  class="text-white/80 text-xl max-w-xl border-t border-t-red-600/80"
-                >
-                  Full extension drawers slide
-                </p>
-              </div>
+          />
+          <div
+            class="absolute h-20 inset-0 !bottom-full top-[calc(100%-5rem)] bg-black/70"
+          >
+            <div class="flex justify-center items-center h-full">
+              <p
+                class="text-white font-bold text-sm max-w-xl w-full relative after:content-[''] after:w-[280px] after:h-[1px] after:bg-[#E41938] after:absolute after:left-0 after:right-0 after:top-0 after:mx-auto text-center pt-2"
+              >
+                Full extension drawers slide
+              </p>
             </div>
-            <!--end grid-->
           </div>
-          <!--end container-->
         </div>
       </div>
-      <!--end Container-->
     </section>
   </section>
 </template>
