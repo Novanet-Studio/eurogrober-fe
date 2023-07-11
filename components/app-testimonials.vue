86e4845d4e81e2a74cde57d857bb2a7cd3f913dc
David Arenas <davejs136@gmail.com>
2023-07-11 11:20:53 -0400

    refactor: testimonial branding styles

--- components/app-testimonials.vue
+++ components/app-testimonials.vue
@@ -1,44 +1,22 @@
 <template>
   <section class="relative md:py-24 py-16">
     <div class="container relative">
-      <div class="grid grid-cols-1 pb-8 text-center">
-        <h6 class="mb-4 text-base font-medium text-indigo-600">
-          We believe in building each other and hence
-        </h6>
-        <h3
-          class="mb-6 md:text-3xl text-2xl md:leading-normal leading-normal font-semibold"
-        >
-          Work with some amazing partners
-        </h3>
-
-        <p class="text-slate-400 max-w-xl mx-auto">
-          Start working with Tailwind CSS that can provide everything you need
-          to generate awareness, drive traffic, connect.
-        </p>
-      </div>
-      <!--end grid-->
-
       <div class="grid relative grid-cols-1 mt-8">
         <div class="tiny-two-item">
           <div class="tiny-slide">
             <div
               class="lg:flex p-6 lg:p-0 relative rounded-md shadow dark:shadow-gray-800 overflow-hidden m-2"
             >
-              <img
-                class="w-24 h-24 lg:w-48 lg:h-auto lg:rounded-none rounded-full mx-auto"
-                src="/images/client/01.jpg"
-                alt=""
-                width="384"
-                height="512"
-              />
               <div class="pt-6 lg:p-6 text-center lg:text-start space-y-4">
-                <p class="text-base text-slate-400">
+                <p class="text-base text-slate-400 italic">
                   " It seems that only fragments of the original text remain in
                   the Lorem Ipsum texts used today. "
                 </p>
 
                 <div>
-                  <span class="text-indigo-600 block mb-1">Thomas Israel</span>
+                  <span class="text-color-4 font-bold block mb-1"
+                    >Thomas Israel</span
+                  >
                   <span class="text-slate-400 text-sm dark:text-white/60 block"
                     >Staff Engineer, Algolia</span
                   >
@@ -46,7 +24,7 @@
               </div>
             </div>
           </div>
-          <div class="tiny-slide">
+          <!-- <div class="tiny-slide">
             <div
               class="lg:flex p-6 lg:p-0 relative rounded-md shadow dark:shadow-gray-800 overflow-hidden m-2"
             >
@@ -71,119 +49,9 @@
                 </div>
               </div>
             </div>
-          </div>
-          <div class="tiny-slide">
-            <div
-              class="lg:flex p-6 lg:p-0 relative rounded-md shadow dark:shadow-gray-800 overflow-hidden m-2"
-            >
-              <img
-                class="w-24 h-24 lg:w-48 lg:h-auto lg:rounded-none rounded-full mx-auto"
-                src="/images/client/03.jpg"
-                alt=""
-                width="384"
-                height="512"
-              />
-              <div class="pt-6 lg:p-6 text-center lg:text-start space-y-4">
-                <p class="text-base text-slate-400">
-                  " One disadvantage of Lorum Ipsum is that in Latin certain
-                  letters appear more frequently than others. "
-                </p>
-
-                <div>
-                  <span class="text-indigo-600 block mb-1"
-                    >Barbara McIntosh</span
-                  >
-                  <span class="text-slate-400 text-sm dark:text-white/60 block"
-                    >Staff Engineer, Algolia</span
-                  >
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="tiny-slide">
-            <div
-              class="lg:flex p-6 lg:p-0 relative rounded-md shadow dark:shadow-gray-800 overflow-hidden m-2"
-            >
-              <img
-                class="w-24 h-24 lg:w-48 lg:h-auto lg:rounded-none rounded-full mx-auto"
-                src="/images/client/04.jpg"
-                alt=""
-                width="384"
-                height="512"
-              />
-              <div class="pt-6 lg:p-6 text-center lg:text-start space-y-4">
-                <p class="text-base text-slate-400">
-                  " Thus, Lorem Ipsum has only limited suitability as a visual
-                  filler for German texts. "
-                </p>
-
-                <div>
-                  <span class="text-indigo-600 block mb-1">Jill Webb</span>
-                  <span class="text-slate-400 text-sm dark:text-white/60 block"
-                    >Staff Engineer, Algolia</span
-                  >
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="tiny-slide">
-            <div
-              class="lg:flex p-6 lg:p-0 relative rounded-md shadow dark:shadow-gray-800 overflow-hidden m-2"
-            >
-              <img
-                class="w-24 h-24 lg:w-48 lg:h-auto lg:rounded-none rounded-full mx-auto"
-                src="/images/client/05.jpg"
-                alt=""
-                width="384"
-                height="512"
-              />
-              <div class="pt-6 lg:p-6 text-center lg:text-start space-y-4">
-                <p class="text-base text-slate-400">
-                  " One disadvantage of Lorum Ipsum is that in Latin certain
-                  letters appear more frequently than others. "
-                </p>
-
-                <div>
-                  <span class="text-indigo-600 block mb-1"
-                    >Barbara McIntosh</span
-                  >
-                  <span class="text-slate-400 text-sm dark:text-white/60 block"
-                    >Staff Engineer, Algolia</span
-                  >
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="tiny-slide">
-            <div
-              class="lg:flex p-6 lg:p-0 relative rounded-md shadow dark:shadow-gray-800 overflow-hidden m-2"
-            >
-              <img
-                class="w-24 h-24 lg:w-48 lg:h-auto lg:rounded-none rounded-full mx-auto"
-                src="/images/client/06.jpg"
-                alt=""
-                width="384"
-                height="512"
-              />
-              <div class="pt-6 lg:p-6 text-center lg:text-start space-y-4">
-                <p class="text-base text-slate-400">
-                  " Thus, Lorem Ipsum has only limited suitability as a visual
-                  filler for German texts. "
-                </p>
-
-                <div>
-                  <span class="text-indigo-600 block mb-1">Jill Webb</span>
-                  <span class="text-slate-400 text-sm dark:text-white/60 block"
-                    >Staff Engineer, Algolia</span
-                  >
-                </div>
-              </div>
-            </div>
-          </div>
+          </div> -->
         </div>
       </div>
-      <!--end grid-->
     </div>
-    <!--end container-->
   </section>
 </template>
