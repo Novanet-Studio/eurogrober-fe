c1d4b2016c78596ca2aeba6bc52c4b8be5fb7793
David Arenas <davejs136@gmail.com>
2023-07-11 17:48:36 -0400

    feat: improve best styles for banner

--- components/home/Banner.vue
+++ components/home/Banner.vue
@@ -5,23 +5,25 @@
         <div
           class="absolute inset-0 bg-[url('/images/shop/bg.jpg')] bg-no-repeat md:bg-left bg-center bg-cover"
         ></div>
-        <div class="absolute inset-0 bg-slate-950/30"></div>
+        <div
+          class="absolute inset-0 bg-slate-950/30 backdrop-blur-[2px] hover:backdrop-blur-0"
+        ></div>
         <div class="container relative">
           <div class="grid grid-cols-1">
             <div class="md:text-start text-center">
               <h1
-                class="font-bold text-[#E92946] lg:leading-normal leading-normal text-xl lg:text-5xl mb-2"
+                class="font-extrabold text-color-1 lg:leading-normal leading-normal text-xl lg:text-5xl mb-2"
               >
                 Kitchen hardware at hand!
               </h1>
-              <p class="text-[#D1D3D4] text-lg max-w-xl text-center">
+              <p class="text-color-5 text-lg max-w-xl text-center">
                 We are committed to selling quality and luxury products for
                 kitchens.
               </p>
               <div class="mt-6">
                 <a
                   href=""
-                  class="btn bg-[#E92946] hover:bg-indigo-700 border-[#E92946] hover:border-indigo-700 text-white rounded-full me-2 mt-2"
+                  class="btn bg-color-1 hover:bg-red-800 border-color-1 hover:border-red-800 text-white rounded-full me-2 mt-2"
                 >
                   Learn more
                 </a>
