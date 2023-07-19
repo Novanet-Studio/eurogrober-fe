b053a0717feaadad845d2a713b506f41ee89648d
David Arenas <davejs136@gmail.com>
2023-07-19 16:42:12 -0400

    feat: add grober image

--- components/home/Image.vue
+++ components/home/Image.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="flex justify-center mt-16">
-    <img src="/images/logo-dark.png" alt="" lazy />
+    <img class="w-[30rem] -m-64" src="/images/design/grober-alt.svg" alt="" lazy />
   </div>
 </template>
