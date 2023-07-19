f306c364486ed9ab029af26df10c2bb5c448185a
David Arenas <davejs136@gmail.com>
2023-07-19 17:06:16 -0400

    feat: add footer logos

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -3,23 +3,19 @@
     <div class="py-[30px] px-0 lg:py-10">
       <div class="container relative text-center">
         <div class="flex justify-between items-center">
-          <div class="md:text-start text-center">
-            <p class="mb-0 text-[10px] md:text-sm lg:text-lg">
-              ©
-              {{ new Date().getFullYear() }}
+          <div class="md:text-start text-center lg:flex lg:items-center lg:gap-2">
+            <img class="w-12" src="/images/design/logo-alt-2.svg" alt="Eurogrober Logo">
+            <p class="mb-0 text-[10px] md:text-sm lg:text-base">
               Eurogrober USA LLC
             </p>
           </div>
-          <div class="md:text-start text-center">
-            <p class="mb-0 text-[10px] md:text-sm lg:text-lg">
+          <div class="md:text-start text-center lg:flex lg:items-center lg:gap-2">
+            <p class="mb-0 text-[10px] md:text-sm lg:text-base">
               Developed by:
-              <a
-                href="https://shreethemes.in/"
-                target="_blank"
-                class="text-reset"
-                >Novanet</a
-              >.
             </p>
+            <a href="https://novanet.studio" target="_blank">
+              <img class="w-24" src="/images/design/novanet-logo.svg" alt="Novanet Logo">
+            </a>
           </div>
         </div>
       </div>
