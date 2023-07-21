8ca02d02af30a6c4a6572fce08041f616c7753ba
David Arenas <davejs136@gmail.com>
2023-07-21 17:44:45 -0400

    style: set burger design

--- components/app/Navigation.vue
+++ components/app/Navigation.vue
@@ -4,10 +4,7 @@ provide("isOpen", isOpen);
 </script>
 
 <template>
-  <nav
-    id="topnav"
-    class="defaultscroll is-sticky bg-white dark:bg-slate-900 py-4"
-  >
+  <nav id="topnav" class="defaultscroll is-sticky bg-white dark:bg-slate-900 py-6">
     <div class="container relative h-full">
       <AppLogo />
       <AppMenuBurger />
--- components/home/Banner.vue
+++ components/home/Banner.vue
@@ -1,30 +1,25 @@
 <template>
-  <section class="relative mt-28">
+  <section class="relative mt-24">
     <div class="container-fluid relative">
       <div class="relative py-48 table w-full shadow-md overflow-hidden">
-        <div
-          class="absolute -inset-2 bg-[url('/images/design/banner.jpg')] bg-no-repeat md:bg-left bg-center bg-cover"
-        ></div>
+        <div class="absolute -inset-2 bg-[url('/images/design/banner.jpg')] bg-no-repeat md:bg-left bg-center bg-cover">
+        </div>
         <!-- backdrop-blur-[2px] hover:backdrop-blur-0 -->
         <div class="absolute inset-0 bg-slate-950/30"></div>
         <div class="container relative">
           <div class="grid grid-cols-1 mb-12">
             <div class="text-center md:text-start">
-              <h1
-                class="font-extrabold text-color-1 mb-2 text-2xl md:text-3xl lg:text-5xl lg:leading-normal"
-              >
+              <h1 class="font-extrabold text-color-1 mb-2 text-2xl md:text-3xl lg:text-5xl lg:leading-normal">
                 Kitchen hardware at hand!
               </h1>
               <p
-                class="text-color-5 text-lg max-w-xl text-center md:text-start md:text-xl md:max-w-sm lg:text-3xl lg:max-w-lg"
-              >
+                class="text-color-5 text-lg max-w-xl text-center md:text-start md:text-xl md:max-w-sm lg:text-3xl lg:max-w-lg">
                 We are committed to selling quality and luxury products for
                 kitchens.
               </p>
               <div class="mt-6 md:mt-2">
                 <button
-                  class="btn bg-color-1 text-white rounded-full me-2 mt-2 hover:bg-red-800 border-color-1 hover:border-red-800"
-                >
+                  class="btn bg-color-1 text-white rounded-full me-2 mt-2 hover:bg-red-800 border-color-1 hover:border-red-800">
                   Learn more
                 </button>
               </div>
