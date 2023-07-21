af47dd3981f1432d390e9e1d8886c14e859ec176
David Arenas <davejs136@gmail.com>
2023-07-21 10:05:24 -0400

    style(logo): remove no used styles

--- components/app/Logo.vue
+++ components/app/Logo.vue
@@ -1,5 +1,5 @@
 <template>
   <NuxtLink class="logo h-full" to="/">
-    <img src="/images/logo.svg" class="dark:inline-block w-32 mt-4" alt="" />
+    <img src="/images/logo.svg" class="w-32 mt-4" alt="" />
   </NuxtLink>
 </template>
