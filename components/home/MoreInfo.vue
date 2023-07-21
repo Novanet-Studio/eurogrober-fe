d8d200ce7b37e959bc3de3bef5238e7f5fab0003
David Arenas <davejs136@gmail.com>
2023-07-21 10:15:35 -0400

    style(info): set spaces between text

--- components/home/MoreInfo.vue
+++ components/home/MoreInfo.vue
@@ -3,18 +3,18 @@
     <div class="container relative">
       <div class="grid grid-cols-1 pb-8 text-center">
         <h2
-          class="font-extrabold text-2xl text-color-1 text-center text-balance md:text-[40px] md:leading-10 md:mb-4 lg:text-6xl"
+          class="font-extrabold mb-2 text-2xl text-color-1 text-center text-balance md:text-[40px] md:leading-10 md:mb-4 lg:text-6xl"
         >
           Excellence and <br class="lg:hidden" />
           innovation for kitchens
         </h2>
         <h3
-          class="text-xl font-extrabold text-center text-color-4 md:text-3xl md:mb-4 lg:text-[45px] lg:leading-10"
+          class="text-xl font-extrabold text-center text-color-4 mb-2 md:text-3xl md:mb-4 lg:text-[45px] lg:leading-10"
         >
           To give technical <br class="lg:hidden" />
           solutions to everyday life
         </h3>
-        <span class="text-color-4 md:text-xl md:mb-4"
+        <span class="text-color-4 mb-4 md:text-xl md:mb-4"
           >We discover and optimize spaces filling <br class="lg:hidden" />
           them with functionality.</span
         >
