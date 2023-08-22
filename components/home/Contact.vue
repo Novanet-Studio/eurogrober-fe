a72590c3679d5130078cafbfc2aa4c863c005137
David Arenas <davejs136@gmail.com>
2023-08-22 15:32:05 -0400

    fix: replace form name attr

--- components/home/Contact.vue
+++ components/home/Contact.vue
@@ -70,7 +70,7 @@ const onSubmit = handleSubmit(async (data) => {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({
-        'form-name': 'contact',
+        'form-name': 'contact-form',
         ...data,
       }),
     });
@@ -94,9 +94,9 @@ const onSubmit = handleSubmit(async (data) => {
                 Send us a message
               </h3>
 
-              <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field"
+              <form name="contact-form" method="post" data-netlify="true" data-netlify-honeypot="bot-field"
                 action="/?success=true">
-                <input type="hidden" name="form-name" value="contact" />
+                <input type="hidden" name="form-name" value="contact-form" />
                 <p class="mb-0" id="error-msg"></p>
                 <div id="simple-msg"></div>
                 <div class="grid md:grid-cols-6 md:gap-4 lg:grid-cols-12 lg:gap-6">
